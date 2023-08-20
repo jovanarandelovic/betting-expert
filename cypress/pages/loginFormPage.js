7926819493d87469a4bc9c3ecb434833cb63a1e6
Jovana Ranđelović <jovana.r2011@gmail.com>
2023-08-20 22:12:38 +0200

    move getters and methods from homepage to loginform and apply in tests

---
+++ cypress/pages/loginFormPage.js
@@ -0,0 +1,41 @@
+/// <reference types = "cypress"/>
+
+import HomePageElements from "../elements/home.page.elements";
+
+export default class LoginFormPage {
+  constructor() {
+    this.homePageElements = new HomePageElements();
+  }
+
+  //#region Login form
+  getUsernameInputField() {
+    return cy.get(this.homePageElements.usernameInputFiled);
+  }
+
+  getPasswordInputField() {
+    return cy.get(this.homePageElements.passwordInputField);
+  }
+
+  getLoginButtonOnLoginForm() {
+    return cy.get(this.homePageElements.loginButtonOnLoginForm);
+  }
+  //#endregion
+
+  //#region Actions
+
+  enterUsername(username) {
+    this.getUsernameInputField().type(username);
+  }
+  enterPassword(password) {
+    this.getPasswordInputField().type(password);
+  }
+
+  clickLogin() {
+    this.getLoginButtonOnLoginForm().click();
+  }
+  //#endregion
+
+  //#region Resposnive testing
+
+  //#endregion
+}
