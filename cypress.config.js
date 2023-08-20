370b242a0c80d8fbf99acc8178e6e4fc82dd8ad3
Jovana Ranđelović <jovana.r2011@gmail.com>
2023-08-20 21:52:19 +0200

    update config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  watchForFileChanges: true,
+  defaultCommandTimeout: 5000,
+  viewportHeight: 1920,
+  viewportWidth: 1440,
+  env: {
+    foo: "bar",
+    baz: "quux",
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      //  return require("./cypress/plugins/index.js")(on, config);
     },
+    baseUrl: "https://www.bettingexpert.com/",
+    experimentalRunAllSpecs: true,
   },
 });
