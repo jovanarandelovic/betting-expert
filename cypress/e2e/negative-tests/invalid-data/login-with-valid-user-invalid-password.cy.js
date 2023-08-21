dc57492ec0106670848cffce9acedc3ffe942b28
Jovana Ranđelović <jovana.r2011@gmail.com>
2023-08-21 13:17:31 +0200

    fix: add test case and fix description

--- cypress/e2e/negative-tests/invalid-data/login-with-valid-user-invalid-password.cy.js
+++ cypress/e2e/negative-tests/invalid-data/login-with-valid-user-invalid-password.cy.js
@@ -6,7 +6,7 @@ const homePage = new HomePage();
 const loginForm = new LoginFormPage();
 
 describe("Log in a user to the plaform using the valid user and a password that has less than 8 characters", () => {
-  before(() => {
+  beforeEach(() => {
     // Visit the homepage
     cy.visit("");
     // Accept cookies
@@ -16,8 +16,23 @@ describe("Log in a user to the plaform using the valid user and a password that 
   });
 
   // the test fails due to a bug
-  it("should not log in a user with correct email and wrongly formatted password", () => {
+  it("should not log in a user with correct username and wrongly formatted password", () => {
     loginForm.enterUsername(Cypress.env("validCredentials").username);
+    loginForm.enterPassword(
+      Cypress.env("invalidCredentials").wrongFormatPassword
+    );
+    loginForm.getLoginButtonOnLoginForm().should("be.disabled");
+    loginForm
+      .getInputFieldAlert()
+      .should(
+        "have.text",
+        " Please write a password with 8 or more characters "
+      );
+  });
+
+  // the test fails due to a bug
+  it("should not log in a user with correct email and wrongly formatted password", () => {
+    loginForm.enterUsername(Cypress.env("validCredentials").email);
     loginForm.enterPassword(
       Cypress.env("invalidCredentials").wrongFormatPassword
     );
