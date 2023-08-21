f6d7d8940f569d3d50d179476d71c6801a5e523e
Jovana Ranđelović <jovana.r2011@gmail.com>
2023-08-21 13:49:58 +0200

    fix: add test case

--- cypress/e2e/negative-tests/invalid-data/login-invalid-user-invalid-password.cy.js
+++ cypress/e2e/negative-tests/invalid-data/login-invalid-user-invalid-password.cy.js
@@ -39,4 +39,28 @@ describe("Log in a user to the plaform using the invalid user and the invalid pa
         " Please write a password with 8 or more characters "
       );
   });
+
+  // needs more technical info about username requirements
+  it("should not log in a user with invalid email and invalid password", () => {
+    loginForm.enterUsername(Cypress.env("invalidCredentials").wrongFormatEmail);
+    loginForm.enterPassword(
+      Cypress.env("invalidCredentials").wrongFormatPassword
+    );
+    loginForm.getLoginButtonOnLoginForm().should("be.disabled");
+    loginForm
+      .getInputFieldAlert()
+      .first()
+      .should(
+        "have.text",
+        " Please enter a username with more than two characters. "
+      );
+    // this part of the test fails due to a bug: expected password length is 8 characters, but the error is triggered with 2 or less characters
+    loginForm
+      .getInputFieldAlert()
+      .eq(1)
+      .should(
+        "have.text",
+        " Please write a password with 8 or more characters "
+      );
+  });
 });
